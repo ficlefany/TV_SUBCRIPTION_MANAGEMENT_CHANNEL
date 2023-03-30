88e7231c70aa802e7c6f5092d5c12098f136dce2
Administrator@ttcs-it.ttcsglobal.com <Administrator@ttcs-it.ttcsglobal.com>
2023-03-30 14:50:37 +0200

    Initial commit

---
+++ src/MainScreen.java
@@ -0,0 +1,29 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+        System.out.println("Hello world!");
+    }
+}
+
+/*
+1. A Dstv subscriber registers for the first time and
+the system captures his personal information
+2.New subscription attracts once off installation fee
+which varies with number of Tvs per site
+3.A subscription have the following parameters:
+  -date,sub-cycle(start date -> end date),services
+4.Tv channel have : name,origin,language
+5.Tv channels are classified by categories/pakages
+ie..sports,movies,documentaries
+6.A subscriber can changes service package at each cycle
+7.A subscriber cac subscribe to packages or individual channels
+8. Each bundle/package has a price
+7. Subscriber must pay the fees for the next cycle in advance
+
+Key tasks
+ -Manage Tv channels and packages
+ -Subscription management
+ -Payment and fees managesment
+ -store information for later use
+ */
